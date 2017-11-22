1041d095b7ea54dfbfa2d01c34e3911b580897f3
wedwer666 <mitriucmaria@gmail.com>
2017-11-22 22:04:18 +0200

    I have added start button and 3 sec of animation

--- app/src/main/java/com/example/pam/frameanimationpam4/MainActivity.java
+++ app/src/main/java/com/example/pam/frameanimationpam4/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.pam.frameanimationpam4;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button start = (Button) findViewById(R.id.buttonstart);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                ImageView girl = (ImageView)findViewById(R.id.imageViewgirl);
+                girl.setImageResource(R.drawable.girlwalking);
+                AnimationDrawable girlwalking = (AnimationDrawable)girl.getDrawable();
+                girlwalking.start();
+            }
+        });
     }
 }
